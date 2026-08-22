0a52f6a87edad401af9657b51c79b1beb25a5e97
Eduardo Lira <localboglbogl@gmail.com>
2026-08-22 12:31:04 -0400

    Actualizar cache del menu ramyun

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ramyun-cache-v1";
+const CACHE_NAME = "ramyun-cache-v2";
 
 const urlsToCache = [
   "./",
@@ -11,10 +11,28 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
